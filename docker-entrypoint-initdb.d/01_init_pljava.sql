de2b882c32245a4e7d1620446a8bde398567ce9f
onisuly <onisuly@gmail.com>
2020-11-29 13:31:48 +0800

    Update jvm lib location.

--- docker-entrypoint-initdb.d/01_init_pljava.sql
+++ docker-entrypoint-initdb.d/01_init_pljava.sql
@@ -1,4 +1,4 @@
-SET pljava.libjvm_location TO '/usr/lib/jvm/java-11-openjdk-amd64/lib/server/libjvm.so';
+SET pljava.libjvm_location TO '/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so';
 ALTER DATABASE postgres SET pljava.libjvm_location FROM CURRENT;
 ALTER USER postgres SET SEARCH_PATH TO public,sqlj;
 CREATE EXTENSION pljava;
